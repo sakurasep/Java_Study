865e76f7417a7524b03cd30de7d02fe2cc0dbabe
sakurasep <13562008871@163.com>
2024-03-22 17:40:26 +0800

    关于 Map 的相关代码

--- Map/src/MapTest.java
+++ Map/src/MapTest.java
@@ -1,7 +1,24 @@
+import org.junit.Test;
+
+import java.util.HashMap;
+import java.util.Map;
+
 /**
  * @author sakurasep
  * * @date 2024/3/22
  **/
 public class MapTest {
-    
+    @Test
+    public void test() {
+        // 增
+        Map map = new HashMap();
+
+        map.put("AA", 123);
+        map.put("BB", 13);
+        map.put("CC", 23);
+
+
+    }
+
 }
+
